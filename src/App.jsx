6b8cfe7a9c904760899eaac8b010362d078790c3
John Doe <johndoe@example.com>
2026-04-28 13:07:27 +0700

    feat: set Exercise as homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import UserCards from './pages/UserCards'
+import Exercise from './Exercise'
 import MainLayout from './layouts/MainLayout'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Exercise />} />
         <Route path="/login" element={<Login />} />
         <Route path="/usercards" element={<UserCards />} />
+        <Route path="/exercise" element={<Exercise />} />
         <Route path="/dashboard" element={<MainLayout><Dashboard /></MainLayout>} />
       </Routes>
     </BrowserRouter>
